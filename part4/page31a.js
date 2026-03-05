90b45ac0782ba6a568920ffcdab739a5d1389e13
Yaswanth-modala <rangasrisaiyaswanthmodala@gmail.com>
2026-03-05 12:26:04 +0530

    update

---
+++ part4/page31a.js
@@ -0,0 +1,72 @@
+let customers = [
+  {
+    id: 1,
+    name: "John",
+    balance: 5000,
+    transactions: [],
+  },
+  {
+    id: 2,
+    name: "Amy",
+    balance: 5000,
+    transactions: [],
+  },
+];
+function deposit(id, amount) {
+  customers = customers.map((customer) => {
+    if (customer.id === id) {
+      return {
+        ...customer,
+        balance: customer.balance + amount,
+        transactions: [
+          ...customer.transactions,
+          {
+            type: "credit",
+            amount: amount,
+            date: Date.now(),
+          },
+        ],
+      };
+    } else {
+      return customer;
+    }
+  });
+}
+
+function withdraw(id, amount) {
+    customers = customers.map((customer) => {
+        if (customer.id === id) {
+            if (amount > customer.balance){
+                console.log("Insufficient Balance");
+            }
+            else{
+                return {
+                ...customer,
+                balance: customer.balance - amount,
+                transactions: [
+                    ...customer.transactions,
+          {
+            type: "debit",
+            amount: amount,
+            date: Date.now(),
+          },
+        ],
+
+            }
+            };
+        }
+        else{
+            return customer;
+        }
+    });
+}
+
+function checkBalance(id) {
+    const customer = customers.find((customer) => customer.id === id);
+    console.log(customer);
+}
+
+deposit(1, 1500);
+checkBalance(1)
+deposit(1, 1000);
+checkBalance(1)
