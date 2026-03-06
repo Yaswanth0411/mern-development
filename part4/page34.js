f162b200ed84478264a7df8dbc64d57db3a7661d
Yaswanth-modala <rangasrisaiyaswanthmodala@gmail.com>
2026-03-06 10:50:37 +0530

    update

--- part4/page34.js
+++ part4/page34.js
@@ -3,9 +3,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.listen(8080, () => console.log("server started"));
+
+const users = [
+    {name: "John", email: "john@gmail.com", password: "123"},
+    {name: "Jane", email: "jane@gmail.com", password: "456"}
+];
 app.get("/login", (req,res) => {
     res.render("login");
 });
 app.get("/register", (req,res) => {
     res.render("register");
 });
+app.get("/dashboard", (req,res) => {
+    res.render("dashboard", { users: users });
+})
